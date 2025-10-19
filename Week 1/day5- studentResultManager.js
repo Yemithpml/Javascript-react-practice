a328a546ef06c88c6f0a28baec8ad2194ffd826c
Yemi.dev <opeyemiodusanya60@gmail.com>
2025-10-18 21:34:28 -0700

    Updated day 5 code with appropriate commits where necessary

--- Week 1/day5- studentResultManager.js
+++ Week 1/day5- studentResultManager.js
@@ -7,6 +7,7 @@ const Students = [
     {name : "Sophia", course : "MCH", scores : [92, 91, 52] }
 ]
 
+//destructuring the array of objects and using the spread opeartor
 const withAverage = Students.map(Student => {const{scores, ...rest} = Student
     const Average = scores.reduce((sum, score) => sum + score, 0)/scores.length
     return{...rest, Average}
@@ -14,15 +15,16 @@ const withAverage = Students.map(Student => {const{scores, ...rest} = Student
 
 //console.log(withAverage)
 
-//passed
+//passed [Filters the objects of students who scored 50 and above into a new array]
 const passed = withAverage.filter(({Average}) => Average >= 50);
 console.log(`Passed students: ${passed}`)
 
+//displays each student individually with their values and numbers them accordingly
 passed.forEach(({name, course, Average}, index) => {
     console.log(`${index +1}. ${name} (${course} - ${Average})`)
 })
 
-//Adding new student
+//Adding new student to the initial student array
 
 const newStudent = {name : "David", course: "TCH", scores: [71, 56, 89]}
 const updateStudents = {...Students, newStudent}
